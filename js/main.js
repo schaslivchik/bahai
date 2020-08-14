db25c82779f4c213f6f69113ace55fcddb51654e
schaslivchik <schaslivchikm@gmail.com>
2020-08-14 16:49:18 +0300

    updated

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(window).scroll(function () {
+
+  var scroll = $(window).scrollTop();
+
+  $('.layer').each(function () {
+
+    var layer = $(this);
+
+    var dataSpeed = layer.data('parallax-speed');
+
+    var offsetY = -(scroll * dataSpeed);
+
+    var translate = 'translate3d(0, ' + offsetY + 'px, 0)';
+
+    layer.css('-webkit-transform', translate);
+
+    layer.css('-moz-transform', translate);
+
+    layer.css('-ms-transform', translate);
+
+    layer.css('-o-transform', translate);
+
+    layer.css('transform', translate);
+
+  });
+
+});
